d31d419d9db9e3d3107893156b12adc472b9deda
Sara Salzano <sarasalzano95@gmail.com>
2025-10-16 13:33:29 +0200

    esegui crud

--- routers/postsRouter.js
+++ routers/postsRouter.js
@@ -4,7 +4,34 @@ const express = require("express");
 const router = express.Router();
 
 // index
-app.get('/', function (req, res) {
-res.send('Lista dei post');
+router.get("/", function (req, res) {
+res.send("Lista ricette");
 });
 
+//show
+router.get('/:id', function (req, res) {
+res.send("Dettagli dela ricetta" + req.params.id);
+});
+
+//store
+router.post("/", function (req, res) {
+res.send("Creazione nuova ricetta");
+});
+
+//update
+router.put("/:id", function (req, res) {
+res.send("Modifica ricetta" + req.params.id);
+});
+
+//modify
+router.patch("/:id", function (req, res) {
+res.send('Modifica parziale della ricetta ' + req.params.id);
+});
+
+//destroy
+router.delete("/:id", function (req, res) {
+res.send("Eliminazione della ricetta" + req.params.id);
+});
+
+//export del router 
+module.exports = router;
